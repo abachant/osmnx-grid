e2a189858e4be17732432d4d60096358b54d65fe
gbachant <gbachant@gmail.com>
2018-03-02 00:25:19 -0500

    Add basic ability show edges depending on their bearing

--- app.py
+++ app.py
@@ -14,17 +14,10 @@ G = ox.graph_from_place(place, network_type='bike', simplify=False)
 
 ox.utils.add_edge_bearings(G)
 
-bearing_data_copy = [data['bearing'] for u, v, k, data in G.edges(keys=True, data=True)]
-for i in range(len(bearing_data_copy)):
-    bearing_data_copy[i] = float(bearing_data_copy[i])
-    bearing_data_copy[i] = int(round(bearing_data_copy[i]))
+search_bearing = [220]
 
-d = {'raw_bearing' : pd.Series([data['bearing'] for u, v, k, data in G.edges(keys=True, data=True)]), 'rounded_bearing' : pd.Series(bearing_data_copy)}
+for u,v,a in G.edges(data=True):
+    a['rounded_bearing']=int(round(a['bearing']))
 
-bearings = pd.DataFrame(d)
-
-# ec = ox.get_edge_colors_by_attr(G, attr='bearing')
-# ox.plot_graph(G, edge_color=ec)
-
-# fig, ax = ox.plot_graph(G, node_color='w', node_edgecolor='k', node_size=0,
-#                            node_zorder=3, edge_color=ec, edge_linewidth=3)
+ec = ['r' if data['rounded_bearing'] in search_bearing else 'b' for u, v, key, data in G.edges(keys=True, data=True)]
+fig, ax = ox.plot_graph(G, node_size=0.3, edge_color=ec, edge_linewidth=2.5, edge_alpha=1)
