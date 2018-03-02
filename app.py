649d6c9723a53f2af395a1f260a98ce991c31979
gbachant <gbachant@gmail.com>
2018-03-02 14:59:12 -0500

    Create function for getting perpendiculars of a bearing

--- app.py
+++ app.py
@@ -14,10 +14,19 @@ G = ox.graph_from_place(place, network_type='bike', simplify=False)
 
 ox.utils.add_edge_bearings(G)
 
-search_bearing = [220]
+def add_perpendicular_bearings(search_bearing):
+    for i in range(3):
+        search_bearing.append((search_bearing[-1]+90)%360)
+    return search_bearing
+
+search_bearing = add_perpendicular_bearings([171])
 
 for u,v,a in G.edges(data=True):
     a['rounded_bearing']=int(round(a['bearing']))
+    a['modulo_bearing']=a['rounded_bearing']%90
 
 ec = ['r' if data['rounded_bearing'] in search_bearing else 'b' for u, v, key, data in G.edges(keys=True, data=True)]
 fig, ax = ox.plot_graph(G, node_size=0.3, edge_color=ec, edge_linewidth=2.5, edge_alpha=1)
+
+# for u,v,a in G.edges(data=True):
+#     print(a)
