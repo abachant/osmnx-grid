619355cbf7281201ad0bcd3c7c3b4ebab06cdf57
gbachant <gbachant@gmail.com>
2018-03-02 16:46:42 -0500

    Improve add_perpendicular_bearings to allow multiple bearings

--- app.py
+++ app.py
@@ -13,20 +13,37 @@ area = ox.project_gdf(gdf).unary_union.area
 G = ox.graph_from_place(place, network_type='bike', simplify=False)
 
 ox.utils.add_edge_bearings(G)
-
 def add_perpendicular_bearings(search_bearing):
     for i in range(3):
         search_bearing.append((search_bearing[-1]+90)%360)
     return search_bearing
 
-search_bearing = add_perpendicular_bearings([171])
+def add_perpendicular_bearings(search_bearing):
+    """Take in a single bearing or list of bearings and return all of their parallel and perpendicular bearings in a list"""
+    final_list=[]
+    if type(search_bearing) == list:
+        for i in search_bearing:
+            final_list.append(i)
+            for t in range(3):
+                final_list.append((i+(90*(t+1)))%360)
+    elif type(search_bearing) == int:
+        final_list.append(search_bearing)
+        for t in range(3):
+            final_list.append((search_bearing+(90*(t+1)))%360)
+    else:
+        print("Please make sure the bearing(s) you are searching for are either an integer or a list of integers")
+    return final_list
 
-for u,v,a in G.edges(data=True):
-    a['rounded_bearing']=int(round(a['bearing']))
-    a['modulo_bearing']=a['rounded_bearing']%90
+search_bearing = add_perpendicular_bearings(search_bearing=45)
+def add_more_edge_bearing_info(G):
+    #should go in ox.utils
+    #requires add_edge_bearings() first
+    for u,v,a in G.edges(data=True):
+        a['rounded_bearing']=int(round(a['bearing']))
+        a['modulo_bearing']=a['rounded_bearing']%90
 
-ec = ['r' if data['rounded_bearing'] in search_bearing else 'b' for u, v, key, data in G.edges(keys=True, data=True)]
-fig, ax = ox.plot_graph(G, node_size=0.3, edge_color=ec, edge_linewidth=2.5, edge_alpha=1)
-
+# ec = ['r' if data['rounded_bearing'] in search_bearing else 'b' for u, v, key, data in G.edges(keys=True, data=True)]
+# fig, ax = ox.plot_graph(G, node_size=0.3, edge_color=ec, edge_linewidth=2.5, edge_alpha=1)
+print(search_bearing)
 # for u,v,a in G.edges(data=True):
 #     print(a)
